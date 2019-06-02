c595dc58302a43059bd4ff4c889691456a950c04
Remus Mocanu <Remus_C.M@hotmail.com>
2019-06-02 19:51:29 -0400

    Ex21

---
+++ functionReturn.py
@@ -0,0 +1,8 @@
+#Ex21. Functions can return stuff.
+def add_two_numbers(arg1, arg2):
+    return arg1+arg2
+
+print(add_two_numbers(10,7))
+
+#As in Java, return does not print.
+add_two_numbers(7,10)
