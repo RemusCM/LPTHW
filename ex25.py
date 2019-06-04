fbfad75410969e998f3bb386bb86128a66000166
Remus Mocanu <Remus_C.M@hotmail.com>
2019-06-04 14:24:56 -0400

    Ex25

---
+++ ex25.py
@@ -0,0 +1,18 @@
+#This exercise serve for more practice, only that this time around,
+#we import the program's methods in powershell, and use them there
+#(No calls in the program)
+
+from sys import argv
+
+scriptName = argv
+
+def printThis(stringToPrint):
+    print(stringToPrint)
+
+def mult(numb1, numb2):
+    print(int(numb1)*int(numb2))
+
+"""To call functions in powershell, import ex25 (no need to add
+.py extension)
+Then, whenever you want to call a function, all we need to do is
+ex25.functionName(args)"""
