3665a37ddf962ca9edb0d64ac8f2a8c1320694ff
Remus Mocanu <Remus_C.M@hotmail.com>
2019-06-09 22:33:38 -0400

    ex37-38

--- ex37.py
+++ ex37.py
@@ -79,8 +79,10 @@ try:
         raise WrongFormat("Wrong format.") #use raise where it could possibly fail
                                             #define what to do in case of exception
                                             #in except block.
-except:
+except WrongFormat:
     print("Well hello there, wrong format caught.")
+finally:
+    print("This happens, exceptions or not.")
 
 print("Continuing.")
 
@@ -94,12 +96,12 @@ with openedFile as in_file:
 #yield keyword; used like return, but returns generators, iteratables that can be
 #iterated over only once.
 def createGenerator():
-    mylist = range(3)
+    mylist = range(3) #make a list.
     for i in mylist:
-        yield i*i
+        yield i*i #this is what returns a generator.
 
 myGen = createGenerator()
-print(myGen)
+print(myGen) #prints address
 for i in myGen:
     print(i)
 
@@ -108,3 +110,44 @@ for i in myGen:
 dict = {'a':1, 'b':2}
 print(dict)
 print(dict.get('a'))
+
+#at, decorators, @classmethod, @staticmethod.
+#Class methods can be used to different types of constructors,
+#or setters/getters for an entire class. Ex: imagine increasing
+#interest rate from 4% to 5% for some future clients (not all, the base
+#constructor stays at 4%)
+
+class Client:
+    interest_rate = 1.04
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return 'Name: %s \n Age: %s \n Interest Rate: %s ' % (self.name, self.age, self.interest_rate)
+
+    @classmethod
+    def set_interest_rate(cls,ir):
+        cls.interest_rate = ir
+
+
+
+c1 = Client("Remus", 22)
+
+print(str(c1))
+
+Client.set_interest_rate(1.05)
+
+c2 = Client("Bob", 17)
+
+
+print(c2)
+
+#bytes data type:
+byte = b"\xe4\xb8\xad\xe6\x96\x87"
+
+print(byte)
+
+#if you just want the actual string, need to decode, recall ex23
+print(byte.decode("utf-8"))
---
+++ ex38.py
@@ -0,0 +1,24 @@
+#Ex 38, Doing things to list.
+
+from sys import argv
+
+scriptName = argv
+
+print(f"Welcome to {scriptName}, where we work more on lists.")
+
+print("""
+First, let's learn about the differences between append, extend, and concatenation( + ).
+append adds the elements you want to the end of the list. if you append a list, Then
+it is appended as its own item.
+extend adds items individually ( if you extend a list, then it iterates through
+every item within the list, and appends it to the list)
+concatenation creates a new list with the addition of the selected items to add.
+L = [1,2,3]
+L + [2,4]
+print(L) -> [1,2,3]
+""")
+
+list = [1,2,3]
+
+#join only works on string
+print ("-".join(str(x) for x in list))
